6c0de540190f8282de7d6212927c69ad17ce9f4f
Bill Luo <bill.luo.erp@gmail.com>
2016-01-15 15:12:54 -0800

    scheduler

---
+++ src/main/java/com/rh/invoice/controller/ValidationScheduler.java
@@ -0,0 +1,61 @@
+package com.rh.invoice.controller;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Logger;
+
+import org.apache.activemq.command.ActiveMQDestination;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+
+import com.rh.invoicelog.JmsInt01;
+import com.rh.invoicelog.MessageSender;
+
+
+
+public class ValidationScheduler {
+	Timer timer;
+	static Logger logger = Logger.getLogger(JmsInt01.class.getName());
+	
+	public ValidationScheduler(int minutes) {
+		
+		timer = new Timer();
+		//for test
+		timer.schedule(new ValidationTask(), minutes*1000/60);
+//		timer.schedule(new ValidationTask(), minutes*1000*60);
+	}
+	
+	class ValidationTask extends TimerTask{
+		
+		public void run() {
+			System.out.format("Vendor file validation is running ...");
+	        @SuppressWarnings("resource")
+			ApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/jms-context.xml");
+	 
+	        JmsTemplate template = (JmsTemplate) context.getBean("jmsTemplate");
+	        ActiveMQDestination destination = (ActiveMQDestination) context.getBean("destination");        
+	        //send message
+	        MessageSender messageSender = (MessageSender) context.getBean("messageSender");   
+	        messageSender.sendErrorMessage();
+
+	        // receiving a message
+	        try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        template.receive(destination);
+	        logger.info( " receive is done");
+
+			timer.cancel();			
+		}
+	}
+	
+	public static void main(String args[]) {
+		new ValidationScheduler(60);
+		System.out.format("Task is scheduled%n");
+	}
+
+}
